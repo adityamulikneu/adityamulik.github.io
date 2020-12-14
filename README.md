c19e60d5c6286ffac9477f0a9b9f93332256bcf3
adityamulik <aditya.mulik@gmail.com>
2020-12-14 10:49:35 -0500

    Adding Base React Boilerplate code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Web Application
+
+This project is created as a portfolio or resume of my work by integrating interactive features with help of React JS Framework.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Portfolio Landing Page/ Main Page</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
