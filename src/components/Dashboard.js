c8db18a777817a71409a3bcbac2d0ceda6e40f65
adityamulik <aditya.mulik@gmail.com>
2021-02-21 14:58:16 +0530

    added react-typewriter and react-github-calendar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect/dist/core';
+
+let aboutme = document.getElementById('header-aboutme');
+
+let typewriter = new Typewriter(aboutme, {
+  // strings: ['Software', 'Developer'],
+  wrapperClassName: "new",
+  autoStart: false,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('Hi, I am Aditya Mulik, a')
+  .pauseFor(300)
+  .deleteChars(1)
+  .typeString('<strong></strong>Software Developer! ')
+  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  .pauseFor(1000)
+  .start();
 
 const Home = () => (
   <div className="dashboard">
     <img className="profile-picture" src={process.env.PUBLIC_URL + '/images/aditya.png'} alt="profilePicture" />
     <h1>Aditya Mulik</h1>
+    <h2 id="header-aboutme"></h2>
     <button>Resume</button>
     <button><a href="mailto:aditya.mulik@gmail.com">Email Me</a></button>
   </div>
--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+import ReactTooltip from 'react-tooltip';
 
 const MyWork = () => (
   <div>
+    <GitHubCalendar username="adityamulik" >
+      <ReactTooltip delayShow={50} html />
+    </GitHubCalendar>
   </div>
 );
 
