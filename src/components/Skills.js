3714ae3b580ad9cd59c8fc0e7a451122f4fb1b1a
adityamulik <aditya.mulik@gmail.com>
2021-03-18 21:25:15 +0530

    added dummy d3 graph

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BarChart from "./BarChart/BarChart";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-const Skills = () => (
-  <div style={{textAlign:"center", justifyContent:"center", alignItems:"center"}}>
-    <p style={{textAlign:"center", fontSize: "50px"}}>Work In Progress!!!!!</p>
-  </div>
-);
+const Skills = () => {
+
+  // React hooks with common state values for all components
+  const [selectedGroup, setSelectedGroup] = useState("All");
+  const [groupColour, setGroupColour] = useState("lightgrey");
+
+  return (
+    <div>
+      <svg viewBox="-2 0 100 100" preserveAspectRatio="xMidYMid meet">
+        <BarChart
+          positionX={35}
+          positionY={50}
+          width={80}
+          height={100}
+          selectedGroup={selectedGroup}
+          barColour={groupColour}
+        />
+      </svg>
+    </div>
+  );
+};
 
 export default Skills;
